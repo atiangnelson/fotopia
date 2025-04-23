cc4b4dac740aa35edc923ea8649cbecc6dc34b69
Nelson <nelson.atiang@student.moringaschool.com>
2025-04-23 19:03:50 +0300

    started the fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function App() {
   const[photos,setPhotos]=useState([])
   const[searchPhotos,setSearchPhotos]=useState("")
-  
+  useEffect(()=>{
+    let url=searchPhotos? `https://api.unsplash.com/search/photos?query=${searchPhotos}` : `https://api.unsplash.com/photos/random?count=100`
+    fetch(url,)
+
+  },[searchPhotos])
 
   return (
     <>

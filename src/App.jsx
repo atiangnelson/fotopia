57f6efb12f05748ca27b77377a5e29fda6c03471
Nelson <nelson.atiang@student.moringaschool.com>
2025-04-23 19:19:20 +0300

    finished the fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ function App() {
   const[searchPhotos,setSearchPhotos]=useState("")
   useEffect(()=>{
     let url=searchPhotos? `https://api.unsplash.com/search/photos?query=${searchPhotos}` : `https://api.unsplash.com/photos/random?count=100`
-    fetch(url,)
+    fetch(url,{
+      headers:{
+        Authorization: "Client-ID NU7wvktMWp6YGOH7XNtjZvtIiQczFh82URz5bA6jPl0"
+      }
+    })
+    .then((res)=>res.json())
+    .then((data)=>{
+      if(searchPhotos)
+        setPhotos(data.results)
+      else
+      setPhotos(data)
+    })
+    .catch((err)=>console.error("Error Fetching:",err))
 
   },[searchPhotos])
 

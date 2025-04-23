4012b6e7279e3a146f7bac8ff0415adaa1129f9f
mohamed abass tawana <aminabass987@gmail.com>
2025-04-23 19:16:06 +0300

    iterated every image to display image,title and description

---
+++ src/components/Image.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Image = () => {
+  return (
+    <div className="gen">
+            {photos.map((photo) => (
+                <div key={photo.id}>
+                    <img src={photo.urls.small} alt={photo.alt_description} className="img"/>
+                    <h3>(photo.title)</h3>
+                    <p>{photo.alt_description}</p>
+                </div>
+            )
+        )}
+        </div>
+)}
+
+export default Image

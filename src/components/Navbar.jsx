bf4238f365dc6abd90e708b26e509b09de476d5b
kennedy <kennedy.mwai@student.moringaschool.com>
2025-04-24 20:56:48 +0300

    added a navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <ul>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar

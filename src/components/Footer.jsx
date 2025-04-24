c26ed7fb088cb6cc19444434155cba454c53f8a0
Nelson <nelson.atiang@student.moringaschool.com>
2025-04-24 19:38:02 +0300

    added the heading for the website and info about the website

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+
+    <div className='foot'>
+        <h3 className='abt'>ABOUT THIS SITE</h3>
+        <P className="about">
+
+
+        </P>
+        
+
+    </div>
+  )
+}
+
+export default Footer
